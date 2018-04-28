65cec77831a35c3a860a2f5412d74182cbaa5177
aleksandr-aulov <a.a.aulov@inbox.ru>
2018-04-28 14:29:01 +0300

    person add service

---
+++ rest/src/main/java/com/group/first/app/services/PersonService.java
@@ -0,0 +1,14 @@
+package com.group.first.app.services;
+
+import com.group.first.app.model.Person;
+
+public class PersonService {
+    public void addPerson(Person person){
+
+    }
+
+    public Person getPerson(long id){
+        return null;
+    }
+
+}
