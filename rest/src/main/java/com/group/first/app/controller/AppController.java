38ba0d8573e66f4b40a8e5c2dc0916007bd42133
ganichev-maxim <ganichev_maxim@mail.ru>
2018-04-28 15:41:28 +0300

    personwithcars

--- rest/src/main/java/com/group/first/app/controller/AppController.java
+++ rest/src/main/java/com/group/first/app/controller/AppController.java
@@ -1,16 +1,17 @@
 package com.group.first.app.controller;
 
+import com.group.first.app.exception.PersonIdException;
+import com.group.first.app.facade.PersonFacade;
 import com.group.first.app.facade.StatisticFacade;
 import com.group.first.app.model.Car;
 import com.group.first.app.exception.CarValidateException;
 import com.group.first.app.facade.CarFacade;
+import com.group.first.app.model.PersonWithCars;
 import com.group.first.app.model.Statistics;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Ganichev on 4/28/2018.
@@ -24,6 +25,8 @@ public class AppController {
     @Autowired
     StatisticFacade statisticFacade;
 
+    @Autowired
+    PersonFacade personFacade;
 
 
     @RequestMapping(path = "car", method = RequestMethod.POST)
@@ -55,5 +58,19 @@ public class AppController {
 
     }
 
+    @RequestMapping(path = "personwithcars", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<PersonWithCars> getPersonWithCars(@RequestParam(value = "personid", required = true) Long personId){
+        try {
+            PersonWithCars personWithCars = personFacade.getPersonWithCars(personId);
+            if (personWithCars == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(personWithCars, HttpStatus.OK);
+        } catch (PersonIdException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+    }
 
 }
