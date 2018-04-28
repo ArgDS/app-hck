e680382ab0f437b45b4c1d592bedeb622e500acb
dzhukov40 <dzhukov40.v@mail.ru>
2018-04-28 14:34:47 +0300

    AppController

---
+++ rest/src/main/java/com/group/first/app/model/Statistics.java
@@ -0,0 +1,33 @@
+package com.group.first.app.model;
+
+public class Statistics {
+
+    private Long personcount;
+    private Long carCount;
+    private Long uniquevendorcount;
+
+
+    public Long getPersoncount() {
+        return personcount;
+    }
+
+    public void setPersoncount(Long personcount) {
+        this.personcount = personcount;
+    }
+
+    public Long getCarCount() {
+        return carCount;
+    }
+
+    public void setCarCount(Long carCount) {
+        this.carCount = carCount;
+    }
+
+    public Long getUniquevendorcount() {
+        return uniquevendorcount;
+    }
+
+    public void setUniquevendorcount(Long uniquevendorcount) {
+        this.uniquevendorcount = uniquevendorcount;
+    }
+}
--- rest/src/main/java/com/group/first/app/services/CarService.java
+++ rest/src/main/java/com/group/first/app/services/CarService.java
@@ -1,7 +1,14 @@
 package com.group.first.app.services;
 
+import com.group.first.app.model.Car;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CarService {
+
+    public List<Car> getOunerCars(Long ounerId) {
+        return null;
+    }
 }
