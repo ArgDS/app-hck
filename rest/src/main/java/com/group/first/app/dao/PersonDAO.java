0cc3558108489d5e0a3c632abc55e5a8536c08df
aleksandr-aulov <a.a.aulov@inbox.ru>
2018-04-28 16:46:33 +0300

    dao done

--- rest/src/main/java/com/group/first/app/dao/PersonDAO.java
+++ rest/src/main/java/com/group/first/app/dao/PersonDAO.java
@@ -4,6 +4,7 @@ import com.group.first.app.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.EmptySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -31,11 +32,17 @@ public class PersonDAO {
     }
 
     public void addPerson(Person person){
-
+        String sql = "insert into personal (:id, :name, )";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("id", person.getId());
+        paramMap.put("name", person.getName());
+        paramMap.put("birthdate", person.getBirthdate());
+        npjt.update(sql, paramMap);
     }
 
     public void clearAllPersons(){
-
+        String sql = "delete from person";
+        npjt.update(sql, new HashMap<String, Object>());
     }
 
 }
