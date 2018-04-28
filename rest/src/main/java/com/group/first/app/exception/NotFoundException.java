57335d0780ab109298998c5f4867c4eec9848d50
ganichev-maxim <ganichev_maxim@mail.ru>
2018-04-28 15:22:34 +0300

    personwithcars

---
+++ rest/src/main/java/com/group/first/app/exception/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.group.first.app.exception;
+
+/**
+ * Created by Ganichev on 4/28/2018.
+ */
+public class NotFoundException extends SimpleException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
