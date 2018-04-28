7080b3a738b2c4068bdd71e73a5425d31eb43701
ganichev-maxim <ganichev_maxim@mail.ru>
2018-04-28 17:47:44 +0300

    CarDAO CarService

--- rest/src/main/java/com/group/first/app/services/CarService.java
+++ rest/src/main/java/com/group/first/app/services/CarService.java
@@ -1,6 +1,8 @@
 package com.group.first.app.services;
 
+import com.group.first.app.dao.CarDAO;
 import com.group.first.app.model.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,20 +10,23 @@ import java.util.List;
 @Service
 public class CarService {
 
-    public void addCar(Car car) {
+    @Autowired
+    CarDAO carDAO;
 
+    public void addCar(Car car) {
+        carDAO.addCar(car);
     }
 
     public Car getCar(Long carId) {
-        return null;
+        return carDAO.getCar(carId);
     }
 
     public List<Car> getOunerCars(Long ounerId) {
-        return null;
+        return carDAO.getOunerCars(ounerId);
     }
 
     public void clearAllCars() {
-
+        carDAO.clearAll();
     }
 
 }
