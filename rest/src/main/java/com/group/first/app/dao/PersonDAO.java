6d7bd2934fef7190786c7f74bfb3627fcb86f98c
aleksandr-aulov <a.a.aulov@inbox.ru>
2018-04-28 16:27:32 +0300

    start dao personal

--- rest/src/main/java/com/group/first/app/dao/PersonDAO.java
+++ rest/src/main/java/com/group/first/app/dao/PersonDAO.java
@@ -1,13 +1,33 @@
 package com.group.first.app.dao;
 
 import com.group.first.app.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 public class PersonDAO {
 
+    @Autowired
+    @Qualifier("dataSource")
+    DataSource dataSource;
+    NamedParameterJdbcTemplate npjt;
+
+    public PersonDAO() {
+        npjt = new NamedParameterJdbcTemplate(dataSource);
+    }
+
     public Person getPerson(long id){
-        return null;
+        String sql = "Select * from person where id = :id";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("id", id);
+        return npjt.queryForObject(sql, paramMap, new BeanPropertyRowMapper<Person>());
     }
 
     public void addPerson(Person person){
